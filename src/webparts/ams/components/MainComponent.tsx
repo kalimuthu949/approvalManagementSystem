2b00d06f49a07884ee583d3eefe5ce6b3851e0a2
aravindhvetri <2002aravinthv@gmail.com>
2025-02-10 16:00:03 +0530

    aari's changes 10/02/2025 04:00 pm

---
+++ src/webparts/ams/components/MainComponent.tsx
@@ -0,0 +1,9 @@
+//Default Imports:
+import * as React from "react";
+import { useEffect, useState } from "react";
+
+const MainComponent = ({ context }) => {
+  return <div>aari</div>;
+};
+
+export default MainComponent;
