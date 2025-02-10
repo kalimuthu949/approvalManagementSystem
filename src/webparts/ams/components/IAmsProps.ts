723d636573a2394c45055e76fcbc772e0fa8499b
aravindhvetri <2002aravinthv@gmail.com>
2025-02-10 15:44:12 +0530

    first push

---
+++ src/webparts/ams/components/IAmsProps.ts
@@ -0,0 +1,7 @@
+export interface IAmsProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/ams/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IAmsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'AmsWebPartStrings' {
+  const strings: IAmsWebPartStrings;
+  export = strings;
+}
