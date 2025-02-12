9bc283575a23b03913a0faa2f80e197836e83c57
aravindhvetri <2002aravinthv@gmail.com>
2025-02-12 12:55:31 +0530

    aari's changes 12/02/2025 12:55pm

---
+++ src/CommonServices/CommonTemplates.tsx
@@ -0,0 +1,45 @@
+import { Persona, PersonaSize } from "office-ui-fabric-react";
+import * as React from "react";
+import { IPeoplePickerDetails } from "./Interface";
+export const peoplePickerTemplate = (user: IPeoplePickerDetails) => {
+  return (
+    <>
+      {user && (
+        <div
+          style={{
+            display: "flex",
+            gap: "10px",
+            alignItems: "center",
+          }}
+        >
+          <Persona
+            styles={{
+              root: {
+                margin: "0 !important;",
+                ".ms-Persona-details": {
+                  display: "none",
+                },
+              },
+            }}
+            imageUrl={
+              "/_layouts/15/userphoto.aspx?size=S&username=" + user?.email
+            }
+            title={user?.name}
+            size={PersonaSize.size24}
+          />
+          <p
+            style={{
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              margin: 0,
+            }}
+            className="displayText"
+            title={user?.name}
+          >
+            {user?.name}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
